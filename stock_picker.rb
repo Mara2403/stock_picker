fb39d8122d082f87420cb9abf7ec0814815c3f79
Mara <125482894+Mara2403@users.noreply.github.com>
2023-11-10 12:12:52 +0100

    add stock_picker method

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,8 +1,29 @@
 def stock_picker(array)
-  #find the highest number and it's index
-  max_number = array.max
-  max_number_index = array.index(max_number)
-  p [max_number, max_number_index]
+#for each number in an array find the lowest before him, subtract and compare
+  lowest_before = nil
+
+  highest_subtract_value = 0
+  result_pair = nil
+
+  array.each_with_index do |current_number, index|
+    #use only array elements before the current number
+    sliced_array = array[0...index]
+    #filter out numbers equal or grater to the current number in an array
+    number = sliced_array.select { |num| num < current_number}
+    lowest_before = number.empty? ? nil : number.min
+    # subtract
+    unless lowest_before.nil?
+      subtract = current_number - lowest_before
+      #compare subtract with highest_subtract_value
+      if subtract > highest_subtract_value
+        highest_subtract_value = subtract
+        result_pair = [array.index(lowest_before), array.index(current_number)]
+      end
+    end
+  end
+puts "Highest subtract value is #{highest_subtract_value}"
+puts "The best day to buy is day nr #{result_pair[0]} and best day to sell is day nr #{result_pair[1]} (days starts at 0)"
+return result_pair
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
